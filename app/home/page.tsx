f93574f696dede725bf2b5758404a479fd91db69
Hayk-1997 <abrahamyanhayk01@gmail.com>
2025-04-19 01:28:21 +0400

    Create main layout

---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <p>
+        This is the home page of the Redux Toolkit example app. It demonstrates how to use Redux
+        Toolkit with Next.js.
+      </p>
+    </>
+  );
+}
